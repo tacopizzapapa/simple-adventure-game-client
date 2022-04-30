7ed8fdcbbf5d105375632e56215f58296558a26f
TacoPizzaPapa <tacopizzapapa@gmail.com>
2022-04-29 22:32:29 -0500

    Interim commit of demo game work

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.scss';
-import gameDefinition from './games/simpleDemoGame.json';
+import gameDefinition from './games/stuckInTheSupplyClosetDemoGame.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
